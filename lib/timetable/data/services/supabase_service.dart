001f802f4af7d270a5de2b5de1790a214a8a1e57
satyam06050 <krsatyam0605@gmail.com>
2026-08-13 21:46:25 +0530

    added supabse service

---
+++ lib/timetable/data/services/supabase_service.dart
@@ -0,0 +1,32 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<Map<String, dynamic>> getStudent(int rollNo) async {
+    return await _supabase
+        .from('student')
+        .select('roll_no, section, batch, semester')
+        .eq('roll_no', rollNo)
+        .single();
+  }
+
+  Future<List<Map<String, dynamic>>> getTimetable(int rollNo) async {
+    final student = await getStudent(rollNo);
+
+    final data = await _supabase
+        .from('timetable')
+        .select(
+          'section, batch, semester, '
+          'day, start_time, end_time, '
+          'subject_code, room, faculty',
+        )
+        .eq('section', student['section'])
+        .eq('batch', student['batch'])
+        .eq('semester', student['semester'])
+        .order('day')
+        .order('start_time');
+
+    return List<Map<String, dynamic>>.from(data);
+  }
+}
